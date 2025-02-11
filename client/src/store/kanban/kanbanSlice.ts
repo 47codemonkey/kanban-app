0fb7aff661552c5263d5c5d2fb5072fc04d54f35
Oleksii <47codemonkey@gmail.com>
2025-02-11 21:56:01 +0200

    changes

--- client/src/store/kanban/kanbanSlice.ts
+++ client/src/store/kanban/kanbanSlice.ts
@@ -19,8 +19,10 @@ const initialState: KanbanState = {
   },
 };
 
+const API_URL = process.env.API_URL || 'http://localhost:3000';
+
 export const fetchCards = createAsyncThunk('kanban/fetchCards', async (boardId: string) => {
-  const response = await fetch(`http://localhost:3000/api/cards?boardId=${boardId}`);
+  const response = await fetch(`${API_URL}/api/cards?boardId=${boardId}`);
   if (!response.ok) {
     const errorText = await response.text();
     throw new Error(`Fetch error: ${response.status} - ${errorText}`);
@@ -30,7 +32,7 @@ export const fetchCards = createAsyncThunk('kanban/fetchCards', async (boardId: 
 });
 
 export const addCard = createAsyncThunk('kanban/addCard', async (card: Card) => {
-  const response = await fetch('http://localhost:3000/api/cards', {
+  const response = await fetch(`${API_URL}/api/cards`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(card),
@@ -43,7 +45,7 @@ export const addCard = createAsyncThunk('kanban/addCard', async (card: Card) => 
 });
 
 export const updateCard = createAsyncThunk('kanban/updateCard', async (card: Card) => {
-  const response = await fetch(`http://localhost:3000/api/cards/${card._id}`, {
+  const response = await fetch(`${API_URL}/api/cards/${card._id}`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(card),
@@ -56,7 +58,7 @@ export const updateCard = createAsyncThunk('kanban/updateCard', async (card: Car
 });
 
 export const deleteCard = createAsyncThunk('kanban/deleteCard', async (cardId: string) => {
-  const response = await fetch(`http://localhost:3000/api/cards/${cardId}`, { method: 'DELETE' });
+  const response = await fetch(`${API_URL}/api/cards/${cardId}`, { method: 'DELETE' });
   if (!response.ok) {
     const errorText = await response.text();
     throw new Error(`Delete card error: ${response.status} - ${errorText}`);
@@ -129,141 +131,3 @@ const kanbanSlice = createSlice({
 
 export const { openForm, closeForm, setFormData } = kanbanSlice.actions;
 export default kanbanSlice.reducer;
-
-// import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-// import { Card, CardsState, FormState, FormData } from '../../types';
-
-// interface KanbanState {
-//   cards: CardsState;
-//   form: FormState;
-// }
-
-// const initialState: KanbanState = {
-//   cards: {
-//     'To Do': [],
-//     'In Progress': [],
-//     Done: [],
-//   },
-//   form: {
-//     selectedColumn: null,
-//     editingCard: null,
-//     formData: { title: '', description: '' },
-//   },
-// };
-
-// export const fetchCards = createAsyncThunk('kanban/fetchCards', async () => {
-//   console.log('Dispatching fetchCards, making request to /api/cards');
-//   const response = await fetch('http://localhost:3000/api/cards');
-//   console.log('Response received:', response);
-
-//   if (!response.ok) {
-//     const errorText = await response.text();
-//     console.error('Fetch error:', response.status, errorText);
-//     throw new Error(`Fetch error: ${response.status}`);
-//   }
-
-//   const data = (await response.json()) as Card[];
-//   console.log('Data parsed from response:', data);
-//   return data;
-// });
-
-// export const addCard = createAsyncThunk('kanban/addCard', async (card: Card) => {
-//   const response = await fetch('http://localhost:3000/api/cards', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(card),
-//   });
-//   if (!response.ok) {
-//     const errorText = await response.text();
-//     throw new Error(`Add card error: ${response.status} - ${errorText}`);
-//   }
-//   return (await response.json()) as Card;
-// });
-
-// export const updateCard = createAsyncThunk('kanban/updateCard', async (card: Card) => {
-//   const response = await fetch(`http://localhost:3000/api/cards/${card._id}`, {
-//     method: 'PUT',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(card),
-//   });
-//   if (!response.ok) {
-//     const errorText = await response.text();
-//     throw new Error(`Update card error: ${response.status} - ${errorText}`);
-//   }
-//   return (await response.json()) as Card;
-// });
-
-// export const deleteCard = createAsyncThunk('kanban/deleteCard', async (cardId: string) => {
-//   const response = await fetch(`http://localhost:3000/api/cards/${cardId}`, { method: 'DELETE' });
-//   if (!response.ok) {
-//     const errorText = await response.text();
-//     throw new Error(`Delete card error: ${response.status} - ${errorText}`);
-//   }
-//   return cardId;
-// });
-
-// const kanbanSlice = createSlice({
-//   name: 'kanban',
-//   initialState,
-//   reducers: {
-//     openForm: (state, action: PayloadAction<{ column: string; card?: Card }>) => {
-//       const { column, card } = action.payload;
-//       state.form.selectedColumn = column;
-//       if (card) {
-//         state.form.editingCard = card;
-//         state.form.formData = { title: card.title, description: card.description };
-//       } else {
-//         state.form.editingCard = null;
-//         state.form.formData = { title: '', description: '' };
-//       }
-//     },
-//     closeForm: (state) => {
-//       state.form.selectedColumn = null;
-//       state.form.editingCard = null;
-//     },
-//     setFormData: (state, action: PayloadAction<FormData>) => {
-//       state.form.formData = action.payload;
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder.addCase(fetchCards.fulfilled, (state, action) => {
-//       state.cards['To Do'] = [];
-//       state.cards['In Progress'] = [];
-//       state.cards.Done = [];
-//       action.payload.forEach((card) => {
-//         if (card.status in state.cards) {
-//           state.cards[card.status].push(card);
-//         }
-//       });
-//     });
-
-//     builder.addCase(addCard.fulfilled, (state, action) => {
-//       const newCard = action.payload;
-//       if (newCard.status in state.cards) {
-//         state.cards[newCard.status].push(newCard);
-//       }
-//     });
-
-//     builder.addCase(updateCard.fulfilled, (state, action) => {
-//       const updatedCard = action.payload;
-
-//       Object.keys(state.cards).forEach((column) => {
-//         state.cards[column] = state.cards[column].filter((card) => card._id !== updatedCard._id);
-//       });
-
-//       if (updatedCard.status in state.cards) {
-//         state.cards[updatedCard.status].push(updatedCard);
-//       }
-//     });
-
-//     builder.addCase(deleteCard.fulfilled, (state, action) => {
-//       const cardId = action.payload;
-//       Object.keys(state.cards).forEach((column) => {
-//         state.cards[column] = state.cards[column].filter((card) => card._id !== cardId);
-//       });
-//     });
-//   },
-// });
-
-// export const { openForm, closeForm, setFormData } = kanbanSlice.actions;
-// export default kanbanSlice.reducer;
