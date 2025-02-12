5dc2e3cecc89d96916519da7f2d274287e5f6250
Oleksii <47codemonkey@gmail.com>
2025-02-12 10:41:55 +0200

    changes

--- client/eslint.config.cjs
+++ client/eslint.config.cjs
@@ -1,70 +1,73 @@
-const { defineConfig } = require('eslint-define-config');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const reactPlugin = require('eslint-plugin-react');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+const jsxA11yPlugin = require('eslint-plugin-jsx-a11y');
+const importPlugin = require('eslint-plugin-import');
 
-module.exports = defineConfig({
-  root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'import'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'error',
-    'react/require-default-props': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'off',
-    'import/extensions': 'off',
-    'import/order': 'off',
-    '@typescript-eslint/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'max-len': [
-      'error',
-      {
-        code: 120,
-        ignoreTemplateLiterals: true,
-        ignoreStrings: true,
-        ignoreComments: true,
-        ignoreRegExpLiterals: true,
+module.exports = [
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
       },
-    ],
-    'react/no-unescaped-entities': 'off',
-    'object-curly-newline': 'off',
-    'react-hooks/exhaustive-deps': 'warn',
-    'no-duplicate-imports': 'error',
-    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        process: 'readonly',
       },
     },
-  },
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-  },
-  overrides: [
-    {
-      files: ['src/**/*.{js,jsx,ts,tsx}'],
-      rules: {},
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      import: importPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'jsx-a11y': jsxA11yPlugin,
     },
-  ],
-});
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react/require-default-props': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-unresolved': 'off',
+      'import/extensions': 'off',
+      'import/order': 'off',
+      '@typescript-eslint/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'max-len': [
+        'error',
+        {
+          code: 120,
+          ignoreTemplateLiterals: true,
+          ignoreStrings: true,
+          ignoreComments: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+      'react/no-unescaped-entities': 'off',
+      'object-curly-newline': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-duplicate-imports': 'error',
+      'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+        },
+      },
+    },
+  },
+];
